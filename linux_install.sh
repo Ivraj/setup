8e45778faeeea286b00f8036ba485b108ed1384f
Ivraj <ivraj@msn.com>
2018-03-13 09:42:11 -0400

    Adds tmux install, fixs vim install

--- linux_install.sh
+++ linux_install.sh
@@ -26,15 +26,14 @@ cp -f .gitconfig ~/.gitconfig
 OUT=$?
 if [ $OUT -eq 0 ];then
     echo "##### Done with updating .gitconfig #####"
-    exit
 else
     echo "##### ERROR IN RUNNING cp -f .gitconfig ~/.gitconfig #####"
     exit
 fi
 
-##############
-# VIM INSTALL#
-##############
+###############
+# VIM INSTALL #
+###############
 
 echo "##### Installing vim... #####"
 
@@ -42,14 +41,56 @@ sudo apt-get -y install vim
 
 OUT=$?
 if [ $OUT -eq 0 ];then
-    echo "##### Done with installing vim. Updating configurationss #####"
+    echo "##### Done with installing vim. Updating configurations #####"
+
     rm -rf ~/.vim rm -f ~/.vimrc
     mkdir ~/.vim
     cp .netrwhist ~/.vim/.netwrhist
     cp .vimrc ~/.vim/.vimrc
     cp * ~/.vim 
     cp ~/.vim/.vimrc ~/.vimrc
+    export EDITOR=vim
+
+    echo "##### Done with updating vim configurations #####"
+
+    echo "##### Installing Vundle... #####"
+
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    OUT=$?
+    if [ $OUT -eq 0 ];then
+        vim +PluginInstall +qall
+    else
+        echo "ERROR IN GIT CLONING VUNDLE REPO" 
+        exit
+    fi
 else
     echo "##### ERROR IN RUNNING sudo apt-get -y install vim #####"
     exit
 fi
+
+################
+# TMUX INSTALL #
+################
+
+echo "##### Installing tmux... #####"
+
+sudo apt-get -y install tmux
+
+OUT=$?
+if [ $OUT -eq 0 ];then
+    echo "##### Done with installing tmux. Updating configurations #####"
+    rm -f ~/.tmux.conf
+    cp .tmux.conf ~/.tmux.conf
+    OUT=$?
+    if [ $OUT -eq 0 ]; then
+        echo "##### Done with updating tmux configurations #####"
+    else
+        echo "##### ERROR IN UPDATING TMUX CONFIGURATIONS #####"
+        exit
+    fi
+else
+    echo "##### ERROR IN RUNNING sudo apt-get -y install tmux #####"
+    exit
+fi
+
+echo "##### DONE #####"
